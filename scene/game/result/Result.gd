7d4393967c05e8d01263083ef98490c9d44a8e3e
Tang <forbiddensite403@hotmail.com>
2020-05-02 09:29:31 +0800

    Update result page

--- scene/game/result/Result.gd
+++ scene/game/result/Result.gd
@@ -5,7 +5,7 @@ onready var global = $"/root/Global";
 func _ready():
 	if(global.current_floor > global.quest.level):
 		$"Lbl_Header".text = "Quest Complete!";
-		$"Lbl_Message".text = "You earned \n-- " + str(floor(global.score/10)) + " golds -- \nfrom your adventures!"
+		$"Lbl_Message".text = "Your score is \n-- " + str(global.score) + " -- "
 		return;
 
 func _on_Btn_Return_button_up():
